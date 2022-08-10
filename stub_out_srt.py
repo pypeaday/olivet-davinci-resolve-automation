36cffd439f6334d5e5cc74ac7a767bfb3493f269
Nicholas Payne <nic.payne@protonmail.com>
2022-08-10 06:04:06 -0500

    better script to stub out starting srt files

--- stub_out_srt.py
+++ stub_out_srt.py
@@ -1,19 +1,21 @@
 from pathlib import Path
 
 things_to_strip = [
-    '[Verse 1]',
-    '[Verse 2]',
-    '[Verse 3]',
-    '[Verse 4]',
-    '[Verse 5]',
-    '[Chorus]',
-    '[Bridge]',
+    "[Verse 1]",
+    "[Verse 2]",
+    "[Verse 3]",
+    "[Verse 4]",
+    "[Verse 5]",
+    "[Chorus]",
+    "[Bridge]",
 ]
 
 
 def strip_it(s: str) -> str:
     for t in things_to_strip:
-        s = s.replace(t, '')
+        # first try it with new line
+        s = s.replace(f"{t}\n", "")
+        # s = s.replace(t, "")
     return s
 
 
@@ -23,16 +25,35 @@ def stub_it_out(filepath: str):
 
     new_msg = ""
     for i, group in enumerate(msg.split("\n\n")):
+        group = strip_it(group)
         start_time = f"00:00:{5*(i)},000"
         end_time = f"00:00:{5*(i+1)},000"
         srt_timecode = f"{start_time} --> {end_time}"
-        new_msg += f"{i} \n{srt_timecode} \n{group} \n\n"
+        new_msg += f"{i+1}\n{srt_timecode}\n{group}\n\n"
         # print(f"{new_msg}")
-    Path(filepath.replace("raw-lyrics", "song-subtitles")).write_text(new_msg)
+    Path(filepath.replace("01-raw", "02-stubs")).parent.mkdir(
+        parents=True, exist_ok=True
+    )
+    Path(filepath.replace("01-raw", "03-manual-wip")).parent.mkdir(
+        parents=True, exist_ok=True
+    )
+    Path(filepath.replace("01-raw", "02-stubs")).write_text(new_msg)
+    return new_msg
+
 
 if __name__ == "__main__":
-    stub_it_out("./raw-lyrics/lord-i-need-you.srt")
-    
-
-# 22
-# 00:05:35,291 --> 00:05:45,291
+    date = "20220814"
+    files = [
+        "lord-i-need-you",
+        "how-great-is-your-love",
+        "psalm-45-fairest-of-all",
+        "let-the-nations-be-glad",
+    ]
+    for f in files:
+        filepath = f"./lyrics/01-raw/{date}/{f}.srt"
+        new_file = stub_it_out(f"{filepath}")
+        manual_wip_file = Path(filepath.replace("01-raw", "03-manual-wip"))
+        # if I haven't started working on it yet then stage the file for me to
+        # start
+        if not manual_wip_file.exists():
+            manual_wip_file.write_text(new_file)
