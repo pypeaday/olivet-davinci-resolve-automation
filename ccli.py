3e51b75dfe74a35be4653397797393e353cfdfd0
Pypeaday <pypeaday@pm.me>
2022-10-15 13:04:02 -0500

    updates to ccli.py - will rename

--- ccli.py
+++ ccli.py
@@ -1,12 +1,31 @@
 from pathlib import Path
 
 
-class CCLI:
+class Song:
     def __init__(self, entry: str):
         self._entry = entry.split("\n")
-        self.song = self._entry[0].replace('"', "")
-        self.artist = self._entry[1]
-        self.ccli = self._entry[2]
+        self._song = self._entry[0]
+        self._artist = self._entry[1]
+        self._ccli = self._entry[2]
+        self.song = self._song.replace('"', "")
+        self.artist = self._artist
+        self.ccli = self._ccli
+        self.slug = f"{self.song}.{self.artist}.{self.ccli}".lower().replace(" ", "-")
+        self.raw_lyrics_exist = Path(f"./lyrics/01-raw/{self.slug}").exists()
+        self.stubbed_lyrics_exist = Path(f"./lyrics/02-stubs/{self.slug}").exists()
+
+        self.old_raw_file = Path(f"./lyrics/01-raw/{self._song}")
+
+        if not self.raw_lyrics_exist and self.old_raw_file.exists():
+            print(f"Migrating {self._song} raw and stub to new naming convention")
+            self.__migrate_lyric_file()
+
+    def __migrate_lyric_file(self):
+
+        self.old_raw_file.rename(f"./lyrics/01-raw/{self.slug}")
+        old_stub_file = Path(f"./lyrics/02-stubs/{self._song}")
+        if old_stub_file.exists():
+            old_stub_file.rename(f"./lyrics/02-stubs/{self.slug}")
 
     def __repr__(self):
         return "\n".join(self._entry)
@@ -15,14 +34,14 @@ class CCLI:
         return self.__repr__()
 
 
-class CCLIRepository:
+class SongsRepository:
     def __init__(self):
         self.songs = [
-            CCLI(song) for song in Path("./ccli.txt").read_text().split("\n\n")
+            Song(song) for song in Path("./ccli.txt").read_text().split("\n\n")
         ]
 
     def save(self):
         Path("./ccli.txt").write_text("\n\n".join([str(song) for song in self.songs]))
 
     def add_song(self, song: str, artist: str, ccli_number: str):
-        self.songs.append(CCLI("\n".join([song, artist, ccli_number])))
+        self.songs.append(Song("\n".join([song, artist, ccli_number])))
