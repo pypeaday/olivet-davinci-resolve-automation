65c52500953c184b1794e2de6487c94e155c00c9
Pypeaday <pypeaday@pm.me>
2022-10-18 08:47:39 -0500

    start migrating to frontmatter'd approach in stub-it-out

--- stub_out_srt.py
+++ stub_out_srt.py
@@ -24,37 +24,6 @@ def strip_it(s: str) -> str:
     return s
 
 
-def make_sure_paths_exist(filepath: str):
-    Path(filepath.replace("01-raw", "02-stubs")).mkdir(parents=True, exist_ok=True)
-    Path(filepath.replace("01-raw", "03-manual-wip")).mkdir(parents=True, exist_ok=True)
-
-
-def subtitles_exist(filepath: str, date: str) -> bool:
-
-    name = Path(filepath).stem
-
-    print(f"checking if {name} was already done")
-
-    done_directory = Path("lyrics", "04-done")
-    done_files = [str(x) for x in list(done_directory.glob(f"**/*{name}*"))]
-
-    # Check if the filename for raw subtitles/lyrics for the given date is in a historical folder
-    if any([name in x for x in done_files]):
-        old_filepath = Path([x for x in done_files if name in Path(x).name][0])
-        old_date = old_filepath.parent.name
-        new_filepath = Path(f"./lyrics/04-done/{date}/{Path(filepath).name}")
-
-        # write to a log where the stubbed srt file would otherwise be as a hint to me
-        Path(f"./lyrics/02-stubs/{date}/{name}.log").write_text(
-            f"{name} was already processed on {old_date}.\nThe completed subtitles will be copied to {new_filepath} for convenience of importing and organizing in DaVinci"
-        )
-        print(f"Copying {str(old_filepath)} to {new_filepath}")
-        new_filepath.write_text(old_filepath.read_text())
-        return True
-
-    return False
-
-
 def stub_it_out(filepath: str):
 
     frontmatter, msg = Path(filepath).read_text().split("---")
@@ -71,7 +40,7 @@ def stub_it_out(filepath: str):
         srt_timecode = f"{start_time} --> {end_time}"
         new_msg += f"{i+1}\n{srt_timecode}\n{group}\n\n"
         # print(f"{new_msg}")
-    return new_msg
+    return new_msg, frontmatter
 
 
 if __name__ == "__main__":
@@ -91,6 +60,6 @@ if __name__ == "__main__":
             breakpoint()
             continue
         filepath = str(song.raw_lyrics_file)
-        new_msg = stub_it_out(f"{filepath}")
+        new_msg, frontmatter = stub_it_out(f"{filepath}")
 
         Path(filepath.replace("01-raw", "02-stubs")).write_text(new_msg)
